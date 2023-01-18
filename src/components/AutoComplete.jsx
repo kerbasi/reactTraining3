b106b65515e766439f91af4ac58fd3a7e39bc700
Kirill <kirill.novikov@gmail.com>
2023-01-18 16:52:50 +0300

    Start autocomplete

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,3 +1,7 @@
 ﻿export const AutoComplete = () => {
-  return <div>AutoComplete</div>;
+  return (
+    <div className='w-50 p-5 rounded mx-auto'>
+      <div className='form-floating dropdown'></div>
+    </div>
+  );
 };
--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,4 +1,5 @@
 ﻿import { useState, useEffect } from "react";
+import { BsFillCaretUpFill, BsFillCaretDownFill } from "react-icons/bs";
 import finnHub from "../apis/finnHub";
 
 export const StockList = () => {
@@ -37,5 +38,56 @@ export const StockList = () => {
     };
   }, []);
 
-  return <div>StockList</div>;
+  const changeColor = (change) => {
+    return change > 0 ? "success" : change < 0 ? "danger" : "";
+  };
+
+  const renderIcon = (change) => {
+    return change > 0 ? (
+      <BsFillCaretUpFill />
+    ) : change < 0 ? (
+      <BsFillCaretDownFill />
+    ) : (
+      ""
+    );
+  };
+
+  return (
+    <div>
+      <table className='table table-hover table-striped m5-5'>
+        <thead style={{ color: "rgb(79,89,102)" }}>
+          <tr>
+            <th scope='col'>Name</th>
+            <th scope='col'>Last</th>
+            <th scope='col'>Chg</th>
+            <th scope='col'>Chg%</th>
+            <th scope='col'>High</th>
+            <th scope='col'>Low</th>
+            <th scope='col'>Open</th>
+            <th scope='col'>Pclose</th>
+          </tr>
+        </thead>
+        <tbody>
+          {stock.map((stockData) => {
+            return (
+              <tr className='table-row' style={{ cursor: "pointer" }}>
+                <th scope='row'>{stockData.symbol}</th>
+                <td>{stockData.data.c}</td>
+                <td className={`text-${changeColor(stockData.data.d)}`}>
+                  {stockData.data.d} {renderIcon(stockData.data.d)}
+                </td>
+                <td className={`text-${changeColor(stockData.data.dp)}`}>
+                  {stockData.data.dp} {renderIcon(stockData.data.dp)}
+                </td>
+                <td>{stockData.data.h}</td>
+                <td>{stockData.data.l}</td>
+                <td>{stockData.data.o}</td>
+                <td>{stockData.data.pc}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };

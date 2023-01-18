2507518f1f902c727d445840d2bd2d8f3fb9d715
Kirill <kirill.novikov@gmail.com>
2023-01-18 17:35:48 +0300

    add input

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,7 +1,22 @@
 ﻿export const AutoComplete = () => {
   return (
     <div className='w-50 p-5 rounded mx-auto'>
-      <div className='form-floating dropdown'></div>
+      <div className='form-floating dropdown'>
+        <input
+          type='text'
+          id='search'
+          className='form-control'
+          style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
+          placeholder='Search'
+          autoComplete='off'
+        />
+        <label htmlFor='search'>Search</label>
+        {/* <ul
+          className='dropdown-menu show'
+          style={{ height: "200px", width: "100%", overflowY: "scroll" }}
+        >
+        </ul> */}
+      </div>
     </div>
   );
 };

7932d54c1edfeb9ee17033cbae7d35d562fc1ddd
Kirill Novikov <kirill.novikov@gmail.com>
2023-01-19 08:53:34 +0300

    Add search list fetch and show

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,4 +1,66 @@
-﻿export const AutoComplete = () => {
+﻿import { useState, useEffect } from "react";
+import finnHub from "../apis/finnHub";
+
+export const AutoComplete = () => {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [selected, setSelected] = useState([]);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const renderDropdown = () => {
+    const dropdownClass = search ? "show" : null;
+    return (
+      <ul
+        className={`dropdown-menu ${dropdownClass}`}
+        style={{ height: "200px", width: "100%", overflowY: "scroll" }}
+      >
+        {results.map((result) => {
+          return (
+            <li
+              className='dropdown-item'
+              key={result.symbol}
+              onClick={() => {
+                setSelected((prevResult) => {
+                  console.log(prevResult);
+                  return [...prevResult, result];
+                });
+                setSearch("");
+              }}
+            >
+              {result.description} ({result.symbol})
+            </li>
+          );
+        })}
+      </ul>
+    );
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+    const fetchData = async () => {
+      const response = await finnHub.get("/search", {
+        params: {
+          q: search,
+        },
+      });
+      if (isMounted) {
+        setResults(response.data.result);
+      }
+    };
+
+    if (search.length > 0) {
+      fetchData();
+    } else {
+      setResults([]);
+    }
+    return () => {
+      isMounted = false;
+    };
+  }, [search]);
+
   return (
     <div className='w-50 p-5 rounded mx-auto'>
       <div className='form-floating dropdown'>
@@ -9,13 +71,11 @@
           style={{ backgroundColor: "rgba(145, 158, 171, 0.04)" }}
           placeholder='Search'
           autoComplete='off'
+          value={search}
+          onChange={handleChange}
         />
         <label htmlFor='search'>Search</label>
-        {/* <ul
-          className='dropdown-menu show'
-          style={{ height: "200px", width: "100%", overflowY: "scroll" }}
-        >
-        </ul> */}
+        {renderDropdown()}
       </div>
     </div>
   );

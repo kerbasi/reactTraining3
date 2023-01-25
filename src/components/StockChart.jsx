79308b98e664c608c0eb934bd3161b1e7f4dbf63
Kirill <kirill.novikov@gmail.com>
2023-01-25 17:57:26 +0300

    Add chart, add info

---
+++ src/components/StockChart.jsx
@@ -0,0 +1,92 @@
+﻿import { useState } from "react";
+import Chart from "react-apexcharts";
+
+export const StockChart = ({ symbol, stockData }) => {
+  const { day, week, year } = stockData;
+  const [dateFormat, setDateFormat] = useState("24h");
+
+  const determinateDate = () => {
+    switch (dateFormat) {
+      case "24h":
+        return day;
+      case "7d":
+        return week;
+      case "1y":
+        return year;
+    }
+  };
+
+  const options = {
+    chart: {
+      id: "stock data",
+      animations: {
+        speed: 1300,
+      },
+    },
+    title: {
+      text: symbol,
+      align: "center",
+      style: {
+        fontSize: "24px",
+      },
+    },
+    xaxis: {
+      type: "datetime",
+      labels: {
+        datetimeUTC: false,
+      },
+    },
+    tooltip: {
+      x: {
+        format: "MMM dd HH:MM",
+      },
+    },
+  };
+
+  const series = [
+    {
+      name: symbol,
+      data: determinateDate(),
+    },
+  ];
+
+  const setButtonClass = (button) => {
+    const classes = "btn m-1 ";
+    if (dateFormat === button) {
+      return classes + "btn-primary";
+    }
+    return classes + "btn-outline-primary";
+  };
+
+  return (
+    <div className='mt-5 p-4 shadow-sm bg-white'>
+      <Chart options={options} series={series} type='area' width='100%' />
+      <div>
+        <button
+          className={setButtonClass("24h")}
+          onClick={() => {
+            setDateFormat("24h");
+          }}
+        >
+          24h
+        </button>
+        <button
+          className={setButtonClass("7d")}
+          onClick={() => {
+            setDateFormat("7d");
+          }}
+        >
+          7d
+        </button>
+        <button
+          className={setButtonClass("1y")}
+          onClick={() => {
+            setDateFormat("1y");
+          }}
+        >
+          1y
+        </button>
+      </div>
+    </div>
+  );
+};
--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -1,3 +1,83 @@
-﻿export const StockDetailPage = () => {
-  return <div>StockDetailPage</div>;
+﻿import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import finnHub from "../apis/finnHub";
+import { StockChart } from "../components/StockChart";
+import { StockData } from "../components/StockData";
+
+const formatData = (data) => {
+  return data.t.map((t, index) => {
+    return {
+      x: t * 1000,
+      y: Math.floor(data.c[index]),
+    };
+  });
+};
+
+export const StockDetailPage = () => {
+  const { symbol } = useParams();
+  const [stockData, setStockData] = useState();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const date = new Date();
+      const today = Math.floor(date.getTime() / 1000);
+      let oneDay;
+      if (date.getDay() === 6) {
+        oneDay = today - 24 * 60 * 60 * 2;
+      } else if (date.getDay() === 0) {
+        oneDay = today - 24 * 60 * 60 * 3;
+      } else {
+        oneDay = today - 24 * 60 * 60;
+      }
+      const oneWeek = today - 24 * 60 * 60 * 7;
+      const oneYear = today - 24 * 60 * 60 * 365;
+      try {
+        const responses = await Promise.all([
+          finnHub.get("/stock/candle", {
+            params: {
+              symbol,
+              resolution: 30,
+              from: oneDay,
+              to: today,
+            },
+          }),
+          finnHub.get("/stock/candle", {
+            params: {
+              symbol,
+              resolution: 60,
+              from: oneWeek,
+              to: today,
+            },
+          }),
+          finnHub.get("/stock/candle", {
+            params: {
+              symbol,
+              resolution: "W",
+              from: oneYear,
+              to: today,
+            },
+          }),
+        ]);
+        setStockData({
+          day: formatData(responses[0].data),
+          week: formatData(responses[1].data),
+          year: formatData(responses[2].data),
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [symbol]);
+
+  return (
+    <div>
+      {stockData && (
+        <div>
+          <StockChart stockData={stockData} symbol={symbol} />
+          <StockData symbol={symbol} />
+        </div>
+      )}
+    </div>
+  );
 };
